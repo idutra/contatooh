62228db67b9e831a15f44c7e000e16f8d83062ea
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-10 18:56:08 -0300

    Adicionando middlewares Helmet
    Middlewares que pode garantir alguns aspectos de segurança no header

--- config/express.js
+++ config/express.js
@@ -4,18 +4,21 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+var helmet = require('helmet');
 
 module.exports = function() {
   var app = express();
 
   app.set('port', 3000);
+  app.use(cookieParser());
+  app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-    app.use(cookieParser());
-    app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-    app.use(passport.initialize());
-    app.use(passport.session());
-
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(helmet.xframe());
+  app.use(helmet.xssFilter());
+  app.use(helmet.nosniff());
+  app.disable('x-powered-by');
 
   app.use(express.static('./public'));
   app.set('view engine', 'ejs');
