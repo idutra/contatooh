771d78491e204c99aa85f9df662f54a10e808143
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-10 20:28:02 -0300

    Foi adicionado os plugins do grunt para minificação dos arquivos css,js,ejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,26 @@ module.exports = function (grunt) {
             dist : {
                 src: 'dist'
             }
+        },
+        usemin : {
+            html: 'dist/app/views/**/*.ejs'
+        },
+        useminPrepare: {
+            options: {
+                root: 'dist/public',
+                dest: 'dist/public'
+            },
+            html: 'dist/app/views/**/*.ejs'
         }
     });
 
-    grunt.registerTask('default',['dist']);
+    grunt.registerTask('default',['dist','minifica']);
     grunt.registerTask('dist',['clean','copy']);
+    grunt.registerTask('minifica',['useminPrepare','concat','uglify','cssmin','usemin'])
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
 }
