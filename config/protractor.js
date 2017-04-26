4312f38cd99e42c20f55efc4612977a6a6af1a56
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-26 19:42:50 -0300

    Automatizando a autenticação do usuário

--- config/protractor.js
+++ config/protractor.js
@@ -2,5 +2,8 @@
  * Created by IGOR on 26/04/2017.
  */
 exports.config = {
-    specs: ['../test/e2e/**/*.js']
+    specs: ['../test/e2e/**/*.js'],
+    onPrepare: function () {
+        browser.driver.get('http://localhost:3000');
+    }
 }
