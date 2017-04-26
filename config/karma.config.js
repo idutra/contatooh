fbabd08079af4fa10f23bbce57c12eb5102d2913
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-26 14:44:01 -0300

    atualizando o travis.xml

--- config/karma.config.js
+++ config/karma.config.js
@@ -15,6 +15,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        '../public/vendor/angular/angular.js',
+        '../public/vendor/angular-resource/angular-resource.js',
+        '../public/vendor/angular-route/angular-route.js',
+        '../public/js/main.js',
+        '../public/js/controllers/**/*.js',
+        '../public/js/services/**/*.js',
+        '../test/spec/**/*Spec.js'
     ],
 
 
