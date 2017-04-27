5299d11fee1b1718d40e92453292705f1879ceb5
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-27 16:40:21 -0300

    Realizando a Separação do DDO do template

--- public/js/directives/meus-componentes/meus-componentes.js
+++ public/js/directives/meus-componentes/meus-componentes.js
@@ -12,14 +12,7 @@ angular.module('meusComponentes',[]).directive('meuPainel',function () {
 
     directive.transclude = true;
 
-    directive.template =
-        '<div class="panel panel-default">' +
-        '   <div class="panel-heading">' +
-        '       <h3 class="panel-title">{{titulo}}</h3>' +
-        '   </div>' +
-        '   <div ng-transclude class="panel-body">' +
-        '   </div>' +
-        '</div> ';
+    directive.templateUrl = 'js/directives/meus-componentes/meu-painel.html';
 
     return directive;
 })
