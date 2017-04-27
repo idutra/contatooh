9f5aa1ac779f3719ab43994e0274cd2930960dec
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-27 10:43:40 -0300

    Implementação da automatização dos testes via selenium + protractor+ jasmine.

--- config/protractor.js
+++ config/protractor.js
@@ -4,12 +4,25 @@ exports.config = {
         var dvr = browser.driver;
 
         dvr.get('http://localhost:3000/#/auth').then(function() {
+            console.log('***********************************************************');
+            console.log('[SELENIUM-TEST] Iniciando o processo de autenticação');
+            console.log('[SELENIUM-TEST][AÇÃO] Clicando no link [ENTRAR]');
             dvr.findElement(by.id('entrar')).click();
-            dvr.findElement(by.id('Email')).sendKeys('email-google');
+            console.log('[SELENIUM-TEST][AÇÃO] Informando o e-mail de login');
+            dvr.findElement(by.id('Email')).sendKeys('livromeanstack@gmail.com');
+            console.log('[SELENIUM-TEST][AÇÃO] Clicando no botão [NEXT]');
             dvr.findElement(by.id('next')).click();
-            browser.sleep(1000);//Se faz necessário pois o carregamento dos seletores é mais demorado que a ação do selenium.
-            dvr.findElement(by.id('Passwd')).sendKeys('senha-google');
+            console.log('[SELENIUM-TEST] Aguardando o carregamento da página');
+            dvr.sleep(1000);//Se faz necessário pois o carregamento dos seletores é mais demorado que a ação do selenium.
+            console.log('[SELENIUM-TEST][AÇÃO] Informando a senha');
+            dvr.findElement(by.id('Passwd')).sendKeys('mean123!');
+            console.log('[SELENIUM-TEST][AÇÃO] Clicando no botão [ENTRAR]');
             dvr.findElement(by.id('signIn')).click();
+            console.log('[SELENIUM-TEST] Aguardando o carregamento da próxima etapa');
+            dvr.sleep(5000);
+            console.log('[SELENIUM-TEST][AÇÃO] Clicando no botão [APROVAR]');
+            dvr.findElement(by.id('submit_approve_access')).click();
+            dvr.sleep(1000);
         });
     }
 };
--- test/e2e/contatosPageSpec.js
+++ test/e2e/contatosPageSpec.js
@@ -1,3 +1,27 @@
 /**
  * Created by IGOR on 26/04/2017.
  */
+describe('Página principal', function () {
+    beforeEach(function () {
+        browser.get('http://localhost:3000/#/contatos');
+    });
+    it('Deve estar logado', function () {
+        console.log('***********************************************************');
+        element(by.id('usuario-logado')).getText().then(function (texto) {
+            console.log('[SELENIUM-TEST] Verificando se o usuário está logado');
+            expect(texto.trim().length).toBeGreaterThan(0);
+        });
+    });
+
+    it('Deve remover um contato da lista', function () {
+        console.log('***********************************************************');
+        console.log('[SELENIUM-TEST] Removendo um contato da lista');
+        var totalAntes = element.all(by.repeater('contato in contatos')).count();
+        console.log('[SELENIUM-TEST][AÇÃO] Clicando no botçao [REMOVER]');
+        element(by.repeater('contato in contatos').row(0)).element(by.css('.btn')).click();
+        var totalDepois = element.all(by.repeater('contato in contatos')).count();
+        expect(totalDepois).toBeLessThan(totalAntes);
+
+    });
+
+});
