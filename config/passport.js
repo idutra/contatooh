53f71240c4dc7eb96e1e73ab8386bfe273418316
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-27 10:55:33 -0300

    Atualização das informações do google-api para permitir o OAUTH2

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@
  * Created by IGOR on 30/03/2017.
  */
 var passport = require('passport');
-
 /*Autenticação pelo Git-Hub*/
 //var GitHubStrategy = require('passport-github').Strategy;
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
@@ -14,8 +13,8 @@ module.exports = function () {
     var Usuario = mongoose.model('Usuario');
 
     passport.use(new GoogleStrategy({
-            clientID:     'CIENT_ID',
-            clientSecret: 'CLIENT_SECRET',
+            clientID:     '662541689231-8et1lg5mkah6tu32iemvqumli4n30783.apps.googleusercontent.com',
+            clientSecret: '6pYjclWuygjXBoY77GDMvtfa',
             callbackURL: "http://localhost:3000/auth/google/callback",
             passReqToCallback   : true
         },
@@ -38,43 +37,43 @@ module.exports = function () {
     ));
 
     /* Autenticação via GitHub
-    passport.use(new GitHubStrategy({
-        clientID: 'CLIENT_ID',
-        clientSecret: 'CLIENT_SECRET',
-        callbackURL: 'http://localhost:3000/auth/github/callback'
-    }, function(accessToken, refreshToken, profile, done) {
+     passport.use(new GitHubStrategy({
+     clientID: 'c0381860407ca83165a5',
+     clientSecret: '0ad55fc8845ef5fd929fe913f14967ae138727d7',
+     callbackURL: 'http://localhost:3000/auth/github/callback'
+     }, function(accessToken, refreshToken, profile, done) {
 
-        Usuario.findOrCreate(
-            { "login" : profile.username},
-            { "nome" : profile.username},
-            function(erro, usuario) {
-                if(erro) {
-                    return done(erro);
-                }
-                return done(null, usuario);
-            }
-        );
-    }));*/
+     Usuario.findOrCreate(
+     { "login" : profile.username},
+     { "nome" : profile.username},
+     function(erro, usuario) {
+     if(erro) {
+     return done(erro);
+     }
+     return done(null, usuario);
+     }
+     );
+     }));*/
 
     /*
-       Chamado apenas UMA vez e recebe o usuario do nosso
-       banco disponibilizado pelo callback da estratégia de
-       autenticação. Realizará a serialização apenas no
-       ObjectId do usuário na sessão.
+     Chamado apenas UMA vez e recebe o usuario do nosso
+     banco disponibilizado pelo callback da estratégia de
+     autenticação. Realizará a serialização apenas no
+     ObjectId do usuário na sessão.
      */
     /*
-    passport.serializeUser(function(usuario, done) {
-        done(null, usuario._id);
-    });*/
+     passport.serializeUser(function(usuario, done) {
+     done(null, usuario._id);
+     });*/
     //Recebe o ObjectId do usuário armazenado na sessão
     //Chamado a CADA requisição
     /*
-    passport.deserializeUser(function(id, done) {
-        Usuario.findById(id).exec()
-            .then(function(usuario) {
-                done(null, usuario);
-            });
-    });*/
+     passport.deserializeUser(function(id, done) {
+     Usuario.findById(id).exec()
+     .then(function(usuario) {
+     done(null, usuario);
+     });
+     });*/
 
     passport.serializeUser(function(user, done) {
         done(null, user);
