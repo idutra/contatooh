d09449464ea73591bddc3b5893ec3cb793a2d011
Igor Dutra <igor.p.dutra87@gmail.com>
2017-04-27 09:37:23 -0300

    implementação da configuração para autenticação através do google.

--- config/protractor.js
+++ config/protractor.js
@@ -1,9 +1,15 @@
-/**
- * Created by IGOR on 26/04/2017.
- */
 exports.config = {
     specs: ['../test/e2e/**/*.js'],
-    onPrepare: function () {
-        browser.driver.get('http://localhost:3000');
+    onPrepare: function() {
+        var dvr = browser.driver;
+
+        dvr.get('http://localhost:3000/#/auth').then(function() {
+            dvr.findElement(by.id('entrar')).click();
+            dvr.findElement(by.id('Email')).sendKeys('email-google');
+            dvr.findElement(by.id('next')).click();
+            browser.sleep(1000);//Se faz necessário pois o carregamento dos seletores é mais demorado que a ação do selenium.
+            dvr.findElement(by.id('Passwd')).sendKeys('senha-google');
+            dvr.findElement(by.id('signIn')).click();
+        });
     }
-}
+};
